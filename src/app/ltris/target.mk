272fbc2c0c34d20c6cb48aad82239a7a454713e2
Norman Feske <norman.feske@genode-labs.com>
2015-02-05 21:39:50 +0100

    Ltris - a nice Tetris clone

---
+++ src/app/ltris/target.mk
@@ -0,0 +1,19 @@
+TARGET = ltris
+LIBS   = libc stdcxx sdl sdl_mixer
+
+LTRIS_DIR := $(call select_from_ports,ltris)/src/app/ltris
+
+# determine version by looking at the configure script
+VERSION := $(shell grep "^ VERSION" $(LTRIS_DIR)/configure | sed "s/.*=//")
+
+CC_OPT += -DHI_DIR=\"/var\" -DSRC_DIR=\"/\" \
+          -DVERSION=\"$(VERSION)\"
+
+INC_DIR   += $(LTRIS_DIR)/src
+SRC_C     := $(notdir $(wildcard $(LTRIS_DIR)/src/*.c))
+
+vpath %.c $(LTRIS_DIR)/src
+
+$(TARGET): ltris_data.tar
+ltris_data.tar:
+	$(VERBOSE)cd $(LTRIS_DIR)/src; tar cf $(PWD)/bin/$@ gfx sounds figures
