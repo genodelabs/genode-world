cb1072ea5212e1816ddbea6ccb978635d68f7664
Emery Hemingway <emery@vfemail.net>
2015-11-01 00:00:51 +0100

    Port of OpenTyrian
    https://bitbucket.org/opentyrian/opentyrian
    
    Fixes #4

---
+++ src/app/opentyrian/target.mk
@@ -0,0 +1,21 @@
+TARGET := opentyrian
+
+OPENTYRIAN_DIR := $(call select_from_ports,opentyrian)/src/app/opentyrian
+OPENTYRIAN_SRC := $(OPENTYRIAN_DIR)/src
+
+SRC_C := $(notdir $(wildcard $(OPENTYRIAN_SRC)/*.c))
+
+vpath %.c $(OPENTYRIAN_SRC)
+
+LIBS += libc libm sdl sdl_net
+
+CC_OPT += -std=c99 -DTARGET_UNIX
+
+CC_WARN += -Wno-implicit-function-declaration
+
+.PHONY: tyrian.tar
+
+$(TARGET): tyrian.tar
+
+tyrian.tar:
+	$(VERBOSE) tar cf $@ -C $(OPENTYRIAN_DIR)/tyrian21 .
