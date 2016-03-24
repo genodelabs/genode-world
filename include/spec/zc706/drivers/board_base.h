a4606845e298ec9c8fafa7c4dd42bbd1737a4750
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2016-03-24 17:55:18 +0100

    zynq: add ZC706 and ZC702 specs

--- include/spec/zc706/drivers/board_base.h
+++ include/spec/zc706/drivers/board_base.h
@@ -1,25 +1,25 @@
 /*
- * \brief  Base driver for Parallella Board
+ * \brief  Base driver for ZC706 Board
  * \author Johannes Schlatow
- * \date   2015-06-30
+ * \date   2016-03-24
  */
 
 /*
- * Copyright (C) 2015 Genode Labs GmbH
+ * Copyright (C) 2016 Genode Labs GmbH
  *
  * This file is part of the Genode OS framework, which is distributed
  * under the terms of the GNU General Public License version 2.
  */
 
-#ifndef _INCLUDE__PARALLELLA__DRIVERS__BOARD_BASE_H_
-#define _INCLUDE__PARALLELLA__DRIVERS__BOARD_BASE_H_
+#ifndef _INCLUDE__ZC706__DRIVERS__BOARD_BASE_H_
+#define _INCLUDE__ZC706__DRIVERS__BOARD_BASE_H_
 
 #include <spec/zynq/drivers/board_base_support.h>
 
 namespace Genode { struct Board_base; }
 
 /**
- * Base driver for the Parallella platform
+ * Base driver for the ZC706 platform
  */
 struct Genode::Board_base : Zynq::Board_base
 {
@@ -32,10 +32,10 @@ struct Genode::Board_base : Zynq::Board_base
 		CORTEX_A9_CLOCK             = CPU_6X4X_CLOCK,
 		CORTEX_A9_PRIVATE_TIMER_CLK = CORTEX_A9_CLOCK,
 
-		SDHCI_BASE = SDHCI_1_BASE,
-		SDHCI_SIZE = SDHCI_1_SIZE,
-		SDHCI_IRQ  = SDHCI_1_IRQ,
+		SDHCI_BASE = SDHCI_0_BASE,
+		SDHCI_SIZE = SDHCI_0_SIZE,
+		SDHCI_IRQ  = SDHCI_0_IRQ,
 	};
 };
 
-#endif /* _INCLUDE__PARALLELLA__DRIVERS__BOARD_BASE_H_ */
+#endif /* _INCLUDE__ZC706__DRIVERS__BOARD_BASE_H_ */
