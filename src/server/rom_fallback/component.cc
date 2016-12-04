fb40ccb986a0ac1bf4a2d2a93422f83d686b99f1
Emery Hemingway <emery@vfemail.net>
2016-12-04 12:04:10 +0100

    server/rom_fallback: remove RPC server
    Respond to clients from the "session_requests" ROM dataspace.
    
    Fix #53

--- src/server/rom_fallback/component.cc
+++ src/server/rom_fallback/component.cc
@@ -24,7 +24,7 @@ namespace Rom_fallback {
 	struct Main;
 }
 
-struct Rom_fallback::Main : Rpc_object<Typed_root<Rom_session>>
+struct Rom_fallback::Main
 {
 	struct Fallback_label;
 	typedef List<Fallback_label> Fallback_labels;
@@ -36,20 +36,35 @@ struct Rom_fallback::Main : Rpc_object<Typed_root<Rom_session>>
 
 	Fallback_labels fallbacks;
 
+	struct Session : Parent::Server
+	{
+		Parent::Client parent_client;
+
+		Id_space<Parent::Client>::Element client_id;
+		Id_space<Parent::Server>::Element server_id;
+
+		Session(Id_space<Parent::Client> &client_space,
+		        Id_space<Parent::Server> &server_space,
+		        Parent::Server::Id server_id)
+		:
+			client_id(parent_client, client_space),
+			server_id(*this, server_space, server_id) { }
+	};
+
+	Id_space<Parent::Server> server_id_space;
+
 	Env &env;
 
-	Attached_rom_dataspace config_rom { env, "config" };
+	Attached_rom_dataspace config_rom       { env, "config" };
+	Attached_rom_dataspace session_requests { env, "session_requests" };
 
 	Heap heap { env.ram(), env.rm() };
-	Slab slab { sizeof(Fallback_label), 4096, &heap };
-
-	Parent_service parent_service { "ROM" };
 
 	void load_config()
 	{
 		while (Fallback_label *f = fallbacks.first()) {
 			fallbacks.remove(f);
-			destroy(slab, f);
+			destroy(heap, f);
 		}
 
 		/* keep a pointer to last element of the list for inserting */
@@ -57,38 +72,67 @@ struct Rom_fallback::Main : Rpc_object<Typed_root<Rom_session>>
 		config_rom.xml().for_each_sub_node("fallback", [&] (Xml_node node) {
 			typedef String<Session_label::capacity()> Label;
 
-			Fallback_label *fallback = new (slab)
+			Fallback_label *fallback = new (heap)
 				Fallback_label(node.attribute_value("label", Label()).string());
 			fallbacks.insert(fallback, last);
 			last = fallback;
 		});
 	}
 
-	void update_config()
+	void handle_session_request(Xml_node request);
+
+	void handle_session_requests()
 	{
-		config_rom.update();
-		load_config();
+		if (config_sig_rec.pending()) {
+			do { config_sig_rec.pending_signal(); }
+			while (config_sig_rec.pending());
+			config_rom.update();
+			load_config();
+		}
+
+		session_requests.update();
+
+		Xml_node const requests = session_requests.xml();
+
+		requests.for_each_sub_node([&] (Xml_node request) {
+			handle_session_request(request);
+		});
 	}
 
-	Genode::Signal_handler<Main> update_handler
-		{ env.ep(), *this, &Main::update_config };
+	Signal_handler<Main> session_request_handler {
+		env.ep(), *this, &Main::handle_session_requests };
+
+	Signal_context  config_sig_ctx;
+	Signal_receiver config_sig_rec;
 
 	Main(Genode::Env &env) : env(env)
 	{
-		config_rom.sigh(update_handler);
 		load_config();
 
-		env.parent().announce(env.ep().manage(*this));
+		config_rom.sigh(config_sig_rec.manage(&config_sig_ctx));
+		session_requests.sigh(session_request_handler);
 	}
 
+	~Main() {
+		config_sig_rec.dissolve(&config_sig_ctx); }
+};
 
-	/****************************
-	 ** Root session interface **
-	 ****************************/
 
-	Session_capability session(Session_args const &args,
-	                           Affinity     const &affinity) override
-	{
+void Rom_fallback::Main::handle_session_request(Xml_node request)
+{
+	if (!request.has_attribute("id"))
+		return;
+
+	Parent::Server::Id const server_id { request.attribute_value("id", 0UL) };
+
+	if (request.has_type("create")) {
+
+		if (!request.has_sub_node("args"))
+			return;
+
+		typedef Session_state::Args Args;
+		Args const args = request.sub_node("args").decoded_content<Args>();
+
 		Session_label const original = label_from_args(args.string());
 
 		enum { ARGS_MAX_LEN = 256 };
@@ -107,7 +151,18 @@ struct Rom_fallback::Main : Rpc_object<Typed_root<Rom_session>>
 			                           new_label.string());
 
 			/* try this service */
-			try { return parent_service.session(new_args, affinity); }
+			Session *session = nullptr;
+			try {
+				session = new (heap)
+					Session(env.id_space(), server_id_space, server_id);
+
+				Affinity aff;
+				Session_capability cap = 
+					env.session("ROM", session->client_id.id(), new_args, aff);
+
+				env.parent().deliver_session_cap(server_id, cap);
+				return;
+			}
 
 			catch (Parent::Service_denied) {
 				warning("'", new_label, "' was denied"); }
@@ -120,25 +175,44 @@ struct Rom_fallback::Main : Rpc_object<Typed_root<Rom_session>>
 
 			catch (Service::Quota_exceeded) {
 				warning("'", new_label, "' quota donation was insufficient"); }
+
+			if (session)
+				destroy(heap, session);
 		}
 
 		error("no service found for ROM '", original.string(), "'");
-		throw Root::Unavailable();
+		env.parent().session_response(server_id, Parent::INVALID_ARGS);
 	}
 
-	void upgrade(Session_capability, Upgrade_args const &args) override
-	{
-		warning("discarding rom upgrade '", args.string(), "'");
+	if (request.has_type("upgrade")) {
+
+		server_id_space.apply<Session>(server_id, [&] (Session &session) {
+
+			size_t ram_quota = request.attribute_value("ram_quota", 0UL);
+
+			char buf[64];
+			snprintf(buf, sizeof(buf), "ram_quota=%ld", ram_quota);
+
+			// XXX handle Root::Invalid_args
+			env.upgrade(session.client_id.id(), buf);
+			env.parent().session_response(server_id, Parent::SESSION_OK);
+		});
 	}
 
-	void close(Session_capability session) override
-	{
-		parent_service.close(session);
+	if (request.has_type("close")) {
+		server_id_space.apply<Session>(server_id, [&] (Session &session) {
+			env.close(session.client_id.id());
+			destroy(heap, &session);
+			env.parent().session_response(server_id, Parent::SESSION_CLOSED);
+		});
 	}
-};
+
+}
 
 
 void Component::construct(Genode::Env &env)
 {
 	static Rom_fallback::Main inst(env);
+
+	env.parent().announce("ROM");
 }
