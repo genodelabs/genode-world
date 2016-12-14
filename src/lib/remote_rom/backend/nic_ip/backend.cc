db5a3f6f7830bb39348790e0c690f6a1fd12a020
Norman Feske <norman.feske@genode-labs.com>
2016-12-14 12:13:11 +0100

    remote_rom: fix size_t in placement-new operator

--- src/lib/remote_rom/backend/nic_ip/backend.cc
+++ src/lib/remote_rom/backend/nic_ip/backend.cc
@@ -150,8 +150,7 @@ class Remote_rom::Packet_base : public Ethernet_frame, public Ipv4_packet
 		/**
 		 * Placement new.
 		 */
-		void * operator new(Genode::size_t size, void* addr) {
-			return addr; }
+		void * operator new(__SIZE_TYPE__ size, void* addr) { return addr; }
 
 } __attribute__((packed));
 
