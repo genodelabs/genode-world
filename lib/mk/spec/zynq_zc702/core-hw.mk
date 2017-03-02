22d1140ee934f36e3434ed4be7546f721d3e4577
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2017-03-02 18:22:10 +0100

    zynq: update mk files to 17.02

--- lib/mk/spec/zynq_zc702/core-hw.mk
+++ lib/mk/spec/zynq_zc702/core-hw.mk
@@ -4,12 +4,12 @@
 # \date   2016-05-03
 #
 
-TMP := $(call select_from_repositories,lib/mk/spec/zynq/core.inc)
-BASE_HW_DIR := $(TMP:%lib/mk/spec/zynq/core.inc=%)
+TMP := $(call select_from_repositories,lib/mk/spec/zynq/core-hw.inc)
+BASE_HW_DIR := $(TMP:%lib/mk/spec/zynq/core-hw.inc=%)
 
 # add include paths
 INC_DIR += $(REP_DIR)/src/core/include/spec/xilinx_uartps_1
 INC_DIR += $(REP_DIR)/src/core/include/spec/zynq_zc702
 
 # include less specific configuration
-include $(BASE_HW_DIR)/lib/mk/spec/zynq/core.inc
+include $(BASE_HW_DIR)/lib/mk/spec/zynq/core-hw.inc
