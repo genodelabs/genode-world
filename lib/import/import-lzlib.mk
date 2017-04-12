11956d6e5478900ee280a17d8c7f2e182cd4e179
Emery Hemingway <git@spam.works>
2017-04-11 21:01:44 -0500

    port of Lzlib
    http://www.nongnu.org/lzip/
    
    Ref #70

---
+++ lib/import/import-lzlib.mk
@@ -0,0 +1,3 @@
+LZLIB_DIR := $(call select_from_ports,lzlib)
+
+INC_DIR += $(LZLIB_DIR)/include/lzlib
---
+++ lib/mk/lzlib.mk
@@ -0,0 +1,10 @@
+LZLIB_DIR := $(call select_from_ports,lzlib)
+LZLIB_SRC_DIR := $(LZLIB_DIR)/src/lib/lzlib
+
+LIBS += libc
+
+SRC_C = lzlib.c
+
+INC_DIR += $(LZLIB_SRC_DIR)
+
+vpath %.c $(LZLIB_SRC_DIR)
