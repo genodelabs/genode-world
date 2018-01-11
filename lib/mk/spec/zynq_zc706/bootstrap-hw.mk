47faf8c4f8cbd27a98165979532d955e5d1157dc
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2018-01-11 16:54:43 +0100

    zynq: add SMP support for Zynq-7000 boards

--- lib/mk/spec/zynq_zc706/bootstrap-hw.mk
+++ lib/mk/spec/zynq_zc706/bootstrap-hw.mk
@@ -8,13 +8,13 @@ SRC_S   += bootstrap/spec/arm/crt0.s
 SRC_CC  += bootstrap/spec/arm/cpu.cc
 SRC_CC  += bootstrap/spec/arm/cortex_a9_mmu.cc
 SRC_CC  += bootstrap/spec/arm/pic.cc
-SRC_CC  += bootstrap/spec/zynq_zc706/platform.cc
+SRC_CC  += bootstrap/spec/zynq/platform.cc
 SRC_CC  += hw/spec/arm/arm_v7_cpu.cc
 SRC_CC  += hw/spec/32bit/memory_map.cc
 
-NR_OF_CPUS = 1
+NR_OF_CPUS = 2
 
-vpath bootstrap/spec/zynq_zc706/platform.cc $(REP_DIR)/src/
+vpath bootstrap/spec/zynq/platform.cc $(BASE_HW_DIR)/src/
 
 include $(BASE_HW_DIR)/lib/mk/bootstrap-hw.inc
 
