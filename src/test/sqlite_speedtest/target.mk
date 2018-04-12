e6c31ccc3ec41de8d5b86228e656e8081874578d
Emery Hemingway <ehmry@posteo.net>
2018-04-12 11:24:56 +0200

    Remove SQLite port
    SQLite typically comes as a bundled dependency, any problems with
    the current version of SQLite should be fixed in the Genode native
    libraries used by SQLite.
    
    Fix #102

--- src/test/sqlite_speedtest/target.mk
+++
@@ -1,9 +0,0 @@
-TARGET = test-sqlite_speedtest
-LIBS   = sqlite pthread posix
-SRC_C  = speedtest1.c
-
-SQLITE_DIR = $(call select_from_ports,sqlite)/src/lib/sqlite
-
-vpath %.c $(SQLITE_DIR)/
-
-CC_CXX_WARN_STRICT =
