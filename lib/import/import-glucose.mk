fb4833f972e6f083086e7bf267ded9c477055f6d
Johannes Schlatow <schlatow@ida.ing.tu-bs.de>
2018-06-13 14:59:44 +0200

    ports: minor changes in glucose, lemon and keynote
    - automatically set CC_CXX_WARN_STRICT if libs are imported
    - set default cap quota
    
    Issue #106

--- lib/import/import-glucose.mk
+++ lib/import/import-glucose.mk
@@ -1,2 +1,4 @@
 INC_DIR += $(call select_from_ports,glucose)/include/glucose
 INC_DIR += $(call select_from_ports,glucose)/include/
+
+CC_CXX_WARN_STRICT =
--- lib/import/import-lemon.mk
+++ lib/import/import-lemon.mk
@@ -1,2 +1,4 @@
 INC_DIR += $(call select_from_ports,lemon)/include
 REP_INC_DIR += src/lib/lemon
+
+CC_CXX_WARN_STRICT =
