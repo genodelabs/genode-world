b4b9bf0a4d61dbf563eb013a8ef32561dc322f68
Emery Hemingway <ehmry@posteo.net>
2018-12-04 19:09:04 +0100

    Update crypto++ to version 7.0.0

--- lib/mk/cryptopp.mk
+++ lib/mk/cryptopp.mk
@@ -2,11 +2,13 @@ CRYPTOPP_SRC_DIR = $(call select_from_ports,cryptopp)/src/lib/cryptopp
 
 LIBS += stdcxx
 
-CRYPTOPP_SRC_CC := $(notdir $(wildcard $(CRYPTOPP_SRC_DIR)/*.cpp))
+CRYPTOPP_FILTER := adhoc.cpp test.cpp bench1.cpp bench2.cpp validat0.cpp validat1.cpp validat2.cpp validat3.cpp validat4.cpp datatest.cpp regtest1.cpp regtest2.cpp regtest3.cpp dlltest.cpp fipsalgt.cpp tweetnacl.cpp simon-simd.cpp simon.cpp speck-simd.cpp speck.cpp
 
-CRYPTOPP_FILTER = bench.cpp bench2.cpp test.cpp validat1.cpp
+CRYPTOPP_SRC := $(notdir $(wildcard $(CRYPTOPP_SRC_DIR)/*.cpp))
 
-SRC_CC := $(filter-out $(CRYPTOPP_FILTER),$(CRYPTOPP_SRC_CC))
+SRC_CC += $(filter-out $(CRYPTOPP_FILTER),$(CRYPTOPP_SRC))
+
+CXX_DEF += -DCRYPTOPP_DISABLE_SSSE3
 
 CC_WARN += -Wno-delete-non-virtual-dtor
 
