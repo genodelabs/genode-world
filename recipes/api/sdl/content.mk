cee6b89cce9e13385ac801026a4c5770552df04e
Norman Feske <norman.feske@genode-labs.com>
2019-01-04 11:47:04 +0100

    Add libsdl headers
    This is a follow-up commit to "Port of libsdl, mixer, image, net, ttf".
    Besides adding the missing includes, it adjusts the api recipe.
    
    Issue genodelabs/genode#3100

--- recipes/api/sdl/content.mk
+++ recipes/api/sdl/content.mk
@@ -1,13 +1,19 @@
 MIRROR_FROM_REP_DIR := lib/symbols/sdl lib/import/import-sdl.mk \
-                       lib/mk/mesa_api.mk lib/mk/sdlmain.mk
+                       lib/mk/sdlmain.mk
 
-content: $(MIRROR_FROM_REP_DIR) src/lib/sdl include LICENSE
+MIRROR_FROM_LIBPORTS := lib/mk/mesa_api.mk
+
+content: $(MIRROR_FROM_REP_DIR) $(MIRROR_FROM_LIBPORTS) src/lib/sdl include LICENSE
 
 $(MIRROR_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
 
+$(MIRROR_FROM_LIBPORTS):
+	mkdir -p $(dir $@)
+	cp $(GENODE_DIR)/repos/libports/$@ $@
+
 SDL_PORT_DIR  := $(call port_dir,$(REP_DIR)/ports/sdl)
-MESA_PORT_DIR := $(call port_dir,$(REP_DIR)/ports/mesa)
+MESA_PORT_DIR := $(call port_dir,$(GENODE_DIR)/repos/libports/ports/mesa)
 
 #
 # The Mesa header files needed for SDL_OpenGL are copied as well.
@@ -17,7 +23,7 @@ include:
 	cp -r $(SDL_PORT_DIR)/include/SDL $@/
 	cp -r $(REP_DIR)/include/SDL $@/
 	cp -r $(MESA_PORT_DIR)/include/* $@/
-	cp -r $(REP_DIR)/include/EGL $@/
+	cp -r $(GENODE_DIR)/repos/libports/include/EGL $@/
 
 src/lib/sdl:
 	mkdir -p $@
