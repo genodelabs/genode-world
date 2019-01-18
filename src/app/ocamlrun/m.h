6628d0312691327de8484341511b26bb00b13e1e
Emery Hemingway <ehmry@posteo.net>
2019-01-18 15:14:02 +0100

    Port of OCaml bytecode runtime
    A minimal runtime for executing portable OCaml bytecode.
    A bytecode standard library is not yet provided.

---
+++ src/app/ocamlrun/m.h
@@ -0,0 +1,12 @@
+#define ARCH_SIXTYFOUR
+#define SIZEOF_INT 4
+#define SIZEOF_LONG 4
+#define SIZEOF_PTR 8
+#define SIZEOF_SHORT 2
+#define SIZEOF_LONGLONG 8
+#define INT64_LITERAL(s) s ## LL
+#undef ARCH_BIG_ENDIAN
+#undef ARCH_ALIGN_DOUBLE
+#define ARCH_ALIGN_INT64
+#define PROFINFO_WIDTH 0
+#define FLAT_FLOAT_ARRAY
