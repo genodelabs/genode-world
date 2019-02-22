9fe7643285829ccffbdd1e5a0669b3b2c7f4bc34
Emery Hemingway <ehmry@posteo.net>
2019-02-22 15:59:33 +0100

    Remove rom_fallback server

--- recipes/src/rom_fallback/content.mk
+++
@@ -1,2 +0,0 @@
-SRC_DIR = src/server/rom_fallback
-include $(GENODE_DIR)/repos/base/recipes/src/content.inc
--- src/server/rom_fallback/target.mk
+++
@@ -1,5 +0,0 @@
-TARGET = rom_fallback
-SRC_CC = component.cc
-LIBS   = base
-
-CC_CXX_WARN_STRICT =
