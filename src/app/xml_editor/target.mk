70cc59feef93447a5f88485be224c0e3356af9b2
Emery Hemingway <ehmry@posteo.net>
2019-02-22 16:00:45 +0100

    Remove XML editor

--- src/app/xml_editor/target.mk
+++
@@ -1,5 +0,0 @@
-TARGET = xml_editor
-SRC_CC = component.cc
-LIBS   = base vfs
-
-CC_CXX_WARN_STRICT =
--- src/app/xml_term_edit/target.mk
+++
@@ -1,7 +0,0 @@
-TARGET = xml_term_edit
-SRC_CC = component.cc
-LIBS   = base libc
-
-INC_DIR += $(PRG_DIR) $(call select_from_repositories,src/app/cli_monitor)
-
-CC_CXX_WARN_STRICT =
