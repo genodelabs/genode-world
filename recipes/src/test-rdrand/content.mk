e199117e8f59bde69354417296d61ab92290ddc9
Norman Feske <norman.feske@genode-labs.com>
2019-02-24 14:00:51 +0100

    Build src/test-rdrand archive for non-x86_64
    Include the generic stub in the source archive to avoid build errors on
    architectures other than x86_64.

--- recipes/src/test-rdrand/content.mk
+++ recipes/src/test-rdrand/content.mk
@@ -1,7 +1,8 @@
 SRC_DIR = src/test/rdrand
 include $(GENODE_DIR)/repos/base/recipes/src/content.inc
 
-content: include/spec/x86_64/world/rdrand.h
+MIRROR_FROM_REP_DIR := include/spec/x86_64/world/rdrand.h include/world/rdrand.h
+content: $(MIRROR_FROM_REP_DIR)
 
-include/spec/x86_64/world/rdrand.h:
+$(MIRROR_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
