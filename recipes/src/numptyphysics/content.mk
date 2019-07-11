b2f643e37ffc477a932a8ad552bed63de8c2f8fd
Christian Helmuth <christian.helmuth@genode-labs.com>
2019-07-11 16:17:23 +0200

    numptyphysics: exclude .git directories from src archive

--- recipes/src/numptyphysics/content.mk
+++ recipes/src/numptyphysics/content.mk
@@ -7,8 +7,9 @@ content: $(MIRROR_FROM_PORT_AND_REP_DIR) LICENSE
 PORT_DIR := $(call port_dir,$(REP_DIR)/ports/numptyphysics)
 
 $(MIRROR_FROM_PORT_AND_REP_DIR):
-	mkdir -p $(dir $@)
-	cp -r $(PORT_DIR)/$@ $(dir $@)
+	mkdir -p $@
+	tar -C $(PORT_DIR)/$@ --exclude=.git -cf - . | tar -C $@ -xf -
+
 	$(mirror_from_rep_dir)
 
 LICENSE:
