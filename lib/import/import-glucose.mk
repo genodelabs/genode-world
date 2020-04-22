c548f19c34939bbe1770221b265555e77112b299
Norman Feske <norman.feske@genode-labs.com>
2020-04-22 15:41:54 +0200

    Remove stale port of glucose
    Fixes #178

--- lib/import/import-glucose.mk
+++
@@ -1,4 +0,0 @@
-INC_DIR += $(call select_from_ports,glucose)/include/glucose
-INC_DIR += $(call select_from_ports,glucose)/include/
-
-CC_CXX_WARN_STRICT =
--- src/test/glucose/target.mk
+++
@@ -1,7 +0,0 @@
-TARGET = test-glucose
-LIBS   = posix stdcxx zlib glucose
-SRC_CC = main.cc
-
-CC_OPT += -D__STDC_LIMIT_MACROS -D__STDC_FORMAT_MACROS
-
-CC_CXX_WARN_STRICT =
