b894481513547456321b0bae66eb0b889e7e3e06
Norman Feske <norman.feske@genode-labs.com>
2020-06-12 10:06:20 +0200

    Remove pkg/templeos
    Fixes #212

--- recipes/raw/templeos/content.mk
+++
@@ -1,9 +0,0 @@
-content: TempleOS.ISO TempleOS.vbox
-
-ISO_URL = http://archive.org/download/TempleOS20171215/TempleOS.ISO
-
-TempleOS.ISO: 
-	wget $(ISO_URL) -O $@
-
-TempleOS.vbox:
-	cp $(REP_DIR)/recipes/raw/templeos/$@ $@
