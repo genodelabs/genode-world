751f8070f56efc328056a3e4a9ef9c4d74a3cae9
Norman Feske <norman.feske@genode-labs.com>
2020-06-24 14:41:23 +0200

    Add pkg/drivers_interactive-arndale
    Tested via the framebuffer.run and demo.run scripts on the
    Arndale ISB-BASE-Ver2.0 board.

---
+++ recipes/src/arndale_usb_host_drv/content.mk
@@ -0,0 +1,5 @@
+MIRROR_FROM_REP_DIR := src/drivers/usb_host/spec/arndale \
+                       include/spec/exynos5/regulator \
+                       include/regulator_session
+
+include $(REP_DIR)/recipes/src/usb_host_drv.inc
