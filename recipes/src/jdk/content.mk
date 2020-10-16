f94b339fd5344f32aac4a790d12b6773a61f18b6
Pirmin Duss <pirmin.duss@gapfruit.com>
2020-10-16 19:31:01 +0200

    jdk: replace libc_pipe with vfs_pipe
    This should be in a later master of genode-world

--- recipes/src/jdk/content.mk
+++ recipes/src/jdk/content.mk
@@ -12,9 +12,7 @@ content: $(MIRROR_FROM_REP_DIR)
 $(MIRROR_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
 
-MIRROR_FROM_LIBPORTS := include/libc-plugin \
-                        lib/mk/libc_pipe.mk \
-                        src/lib/libc_pipe
+MIRROR_FROM_LIBPORTS := include/libc-plugin
 
 content: $(MIRROR_FROM_LIBPORTS)
 
