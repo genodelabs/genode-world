0626b899fe5fd82e545474fa20cc7c77ea80bad0
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2021-05-19 17:47:30 +0200

    add vncserver test and run script

--- src/test/vncserver/target.mk
+++ src/test/vncserver/target.mk
@@ -1,3 +1,7 @@
 TARGET = test-vncserver
-LIBS   = libc vncserver
-SRC_CC = main.cc
+LIBS   = libc vncserver zlib posix
+
+INC_DIR  += $(LIBVNC_PORT_DIR)/src/lib/vnc/examples
+SRC_C = example.c
+
+vpath example.c $(LIBVNC_PORT_DIR)/src/lib/vnc/examples
