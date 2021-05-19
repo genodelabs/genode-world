6654fff315651b87818f04d9e89b08f84587d1b4
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2021-05-19 16:16:23 +0200

    add vncserver library

---
+++ src/test/vncserver/main.cc
@@ -0,0 +1,16 @@
+/*
+ * \brief  Link test
+ * \author Norman Feske
+ * \date   2009-10-16
+ */
+
+/*
+ * Copyright (C) 2009-2017 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#include <libc/component.h>
+
+void Libc::Component::construct(Libc::Env &) { }
