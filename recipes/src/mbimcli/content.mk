9e37d7df9874fcac886b587f359ca635dd321301
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2021-06-16 16:57:23 +0200

    depot: add archives for mbimcli

---
+++ recipes/src/mbimcli/content.mk
@@ -0,0 +1,18 @@
+content: src/app/mbimcli src/lib/libmbim LICENSE
+
+PORT_DIR := $(call port_dir,$(REP_DIR)/ports/libmbim)
+
+src/app/mbimcli:
+	mkdir -p $@
+	cp -r $(REP_DIR)/$@/* $@/.
+	cp $(PORT_DIR)/src/lib/libmbim/src/mbimcli/*.c $@/.
+	echo "LIBS += base" >> $@/target.mk
+
+src/lib/libmbim:
+	mkdir -p $@/src/
+	cp -r $(REP_DIR)/$@/* $@/
+	cp -r $(PORT_DIR)/src/lib/libmbim/src/mbimcli $@/src/
+	cp -r $(PORT_DIR)/src/lib/libmbim/src/common $@/src/
+
+LICENSE:
+	cp $(PORT_DIR)/src/lib/libmbim/COPYING $@
