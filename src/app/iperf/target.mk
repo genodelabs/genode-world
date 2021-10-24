577f1c047532774bc53cd5cf21e92cc0a69fcf5e
Tomasz Gajewski <tomga@wp.pl>
2021-10-25 00:11:38 +0200

    app/iperf: fix to not change contrib content
    Issue #275

--- src/app/iperf/target.mk
+++ src/app/iperf/target.mk
@@ -4,38 +4,38 @@ IPERF_DIR := $(call select_from_ports,iperf)/src/app/iperf
 
 LIBS += libc libm stdcxx posix
 
-SRC_CC_COMPAT   = $(IPERF_DIR)/compat/Thread.c
-SRC_CC_COMPAT  += $(IPERF_DIR)/compat/error.c
-SRC_CC_COMPAT  += $(IPERF_DIR)/compat/delay.c
-SRC_CC_COMPAT  += $(IPERF_DIR)/compat/gettimeofday.c
-SRC_CC_COMPAT  += $(IPERF_DIR)/compat/inet_ntop.c
-SRC_CC_COMPAT  += $(IPERF_DIR)/compat/inet_pton.c
-SRC_CC_COMPAT  += $(IPERF_DIR)/compat/signal.c
-SRC_CC_COMPAT  += $(IPERF_DIR)/compat/snprintf.c
-SRC_CC_COMPAT  += $(IPERF_DIR)/compat/string.c
+SRC_CC_COMPAT   = compat/Thread.c
+SRC_CC_COMPAT  += compat/error.c
+SRC_CC_COMPAT  += compat/delay.c
+SRC_CC_COMPAT  += compat/gettimeofday.c
+SRC_CC_COMPAT  += compat/inet_ntop.c
+SRC_CC_COMPAT  += compat/inet_pton.c
+SRC_CC_COMPAT  += compat/signal.c
+SRC_CC_COMPAT  += compat/snprintf.c
+SRC_CC_COMPAT  += compat/string.c
 
-SRC_CC_IPERF    = $(IPERF_DIR)/src/Client.cpp
-SRC_CC_IPERF   += $(IPERF_DIR)/src/Extractor.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/isochronous.cpp
-SRC_CC_IPERF   += $(IPERF_DIR)/src/Launch.cpp
-SRC_CC_IPERF   += $(IPERF_DIR)/src/List.cpp
-SRC_CC_IPERF   += $(IPERF_DIR)/src/Listener.cpp
-SRC_CC_IPERF   += $(IPERF_DIR)/src/Locale.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/PerfSocket.cpp
-SRC_CC_IPERF   += $(IPERF_DIR)/src/ReportCSV.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/ReportDefault.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/Reporter.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/Server.cpp
-SRC_CC_IPERF   += $(IPERF_DIR)/src/Settings.cpp
-SRC_CC_IPERF   += $(IPERF_DIR)/src/SocketAddr.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/gnu_getopt.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/gnu_getopt_long.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/histogram.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/main.cpp
-SRC_CC_IPERF   += $(IPERF_DIR)/src/sockets.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/stdio.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/tcp_window_size.c
-SRC_CC_IPERF   += $(IPERF_DIR)/src/pdfs.c
+SRC_CC_IPERF    = src/Client.cpp
+SRC_CC_IPERF   += src/Extractor.c
+SRC_CC_IPERF   += src/isochronous.cpp
+SRC_CC_IPERF   += src/Launch.cpp
+SRC_CC_IPERF   += src/List.cpp
+SRC_CC_IPERF   += src/Listener.cpp
+SRC_CC_IPERF   += src/Locale.c
+SRC_CC_IPERF   += src/PerfSocket.cpp
+SRC_CC_IPERF   += src/ReportCSV.c
+SRC_CC_IPERF   += src/ReportDefault.c
+SRC_CC_IPERF   += src/Reporter.c
+SRC_CC_IPERF   += src/Server.cpp
+SRC_CC_IPERF   += src/Settings.cpp
+SRC_CC_IPERF   += src/SocketAddr.c
+SRC_CC_IPERF   += src/gnu_getopt.c
+SRC_CC_IPERF   += src/gnu_getopt_long.c
+SRC_CC_IPERF   += src/histogram.c
+SRC_CC_IPERF   += src/main.cpp
+SRC_CC_IPERF   += src/sockets.c
+SRC_CC_IPERF   += src/stdio.c
+SRC_CC_IPERF   += src/tcp_window_size.c
+SRC_CC_IPERF   += src/pdfs.c
 
 SRC_CC = $(SRC_CC_COMPAT) $(SRC_CC_IPERF)
 
@@ -50,5 +50,5 @@ CC_WARN = -Wall -Wno-unused -Wno-maybe-uninitialized -Wno-format-truncation \
 CC_CXX_WARN_STRICT =
 
 vpath timer.cc $(PRG_DIR)
-vpath %.c      $(IPERF_DIR)/compat
-vpath %.c      $(IPERF_DIR)/src
+vpath %.cpp    $(IPERF_DIR)
+vpath %.c      $(IPERF_DIR)
