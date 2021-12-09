4ae5f915e398f7ea6476757af4fd9e3acbcb6603
Norman Feske <norman.feske@genode-labs.com>
2021-12-09 14:06:57 +0100

    Remove Ada support
    The broken build remained unattended for more than a year.
    
    Fixes #172

--- src/test/ada/except.ads
+++
@@ -1,8 +0,0 @@
-package Except
-is
-   Test_Exception : exception;
-
-   procedure Do_Something;
-   procedure Do_Sth_2;
-
-end Except;
--- src/test/ada/target.mk
+++
@@ -1,6 +0,0 @@
-TARGET   = test-ada
-SRC_ADB  = main.adb except.adb
-
-SRC_CC   = startup.cc
-LIBS     = libc ada
-INC_DIR += $(PRG_DIR)
