6ef8bb3b313047040d4192db88d8eadb1fa753ab
tor-m6 <tor@innopolis.ru>
2021-12-17 01:21:55 +0300

    libatomic from gcc - port as static lib

---
+++ lib/import/import-libatomic.mk
@@ -0,0 +1,3 @@
+LIBATOMIC_PORT_DIR := $(call select_from_ports,libatomic)
+#INC_DIR += $(LIBBACKTRACE_PORT_DIR)/include
+#INC_DIR += $(LIB_CACHE_DIR)/libatomic/include
---
+++ lib/mk/libatomic.mk
@@ -0,0 +1,3 @@
+LIBATOMIC_DIR := $(call select_from_ports,libatomic)/src/lib/libatomic
+
+LIBS += libc
