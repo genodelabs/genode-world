0c882e266279e94b5775ba3303f08fc0781fea4b
tor-m6 <tor@innopolis.ru>
2022-01-30 23:49:29 +0300

    update .go build system to goroot port

--- src/test/go_pkgs/target.mk
+++ src/test/go_pkgs/target.mk
@@ -1,13 +1,5 @@
 SRC_GO_PKG := greet.go srv.go
 #im.go
-#
-
-MPRG_REL_DIR := $(subst $(REP_DIR)/src/,,$(PRG_DIR))
-MPRG_REL_DIR := $(MPRG_REL_DIR:/=)
-GO_PKG_BUILD := $(BUILD_BASE_DIR)/$(MPRG_REL_DIR)
-SRCPATH      := $(REP_DIR)/src/$(MPRG_REL_DIR)/src
-
-#include $(PRG_DIR)/gobase.mk
 
 include $(REP_DIR)/lib/mk/gobuild.inc
 
