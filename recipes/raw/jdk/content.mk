b1d7e71e9e99165d174ea04fd812c7a7fbbc97c3
Sebastian Sumpf <sebastian.sumpf@genode-labs.com>
2022-02-09 17:50:26 +0100

    jdk: Revive boot2java scenario
    * enable for x86_64 Qemu with two NICs
    * use modular Sculpt runtime approach
    * add test that downloads and checks one http server to the run script
    
    ! make KERNEL=nova BOARD=pc SCULPT=boot2java LOG=core run/boot2java
    
    fixes #284

--- recipes/raw/jdk/content.mk
+++ recipes/raw/jdk/content.mk
@@ -1,4 +1,4 @@
-content: tzdb.dat hello.tar classes.tar
+content: tzdb.dat hello.tar classes.tar servlet.tar 'Genode\ -\ Genode\ News.htm'
 
 GENERATED_DIR := $(call port_dir,$(REP_DIR)/ports/jdk_generated)/src/app/jdk/bin
 
@@ -9,4 +9,10 @@ hello.tar:
 	cp $(GENERATED_DIR)/$@ $@
 
 classes.tar:
+	cp $(GENERATED_DIR)/$@ $@
+
+servlet.tar:
+	cp $(GENERATED_DIR)/$@ $@
+
+'Genode\ -\ Genode\ News.htm':
 	cp $(GENERATED_DIR)/$@ $@
