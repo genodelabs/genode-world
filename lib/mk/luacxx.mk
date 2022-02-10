d97fd26115770d187273abbf94a7e25d134bf04a
Christian Helmuth <christian.helmuth@genode-labs.com>
2022-02-10 08:26:57 +0100

    Import lua/moon from genode repo
    Issue genodelabs/genode#4414
    Fixes #282

---
+++ lib/mk/luacxx.mk
@@ -0,0 +1,13 @@
+#
+# Lua library (C++ variant)
+#
+
+include $(REP_DIR)/lib/mk/lua.inc
+
+SRC_C = $(LUA_CORE_C) $(LUA_LIB_C)
+
+# force compilation with C++ compiler
+CUSTOM_CC = $(CXX)
+CC_WARN  += -Wno-sign-compare
+
+CC_CXX_WARN_STRICT =
---
+++ src/test/moon/target.mk
@@ -0,0 +1,5 @@
+TARGET = test-moon
+LIBS   = luacxx libc base
+SRC_CC = main.cc
+
+CC_CXX_WARN_STRICT =
