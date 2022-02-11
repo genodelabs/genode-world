6f35e248c95b919fb2e410b52f929791241c9e64
Norman Feske <norman.feske@genode-labs.com>
2022-02-11 14:17:46 +0100

    Remove platform support for OMAP4 and Exynos5
    Fixes #283

--- recipes/src/panda_usb_host_drv/content.mk
+++
@@ -1,2 +0,0 @@
-MIRROR_FROM_REP_DIR := src/drivers/usb_host/spec/panda
-include $(REP_DIR)/recipes/src/usb_host_drv.inc
--- src/drivers/sd_card/omap4/target.mk
+++
@@ -1,9 +0,0 @@
-TARGET   = omap4_sd_card_drv
-REQUIRES = arm_v7
-SRC_CC  += main.cc driver.cc
-LIBS    += base
-
-INC_DIR += $(PRG_DIR) $(BASE_DIR)/../os/src/drivers/sd_card
-
-vpath %.cc $(BASE_DIR)/../os/src/drivers/sd_card
-vpath %.cc $(PRG_DIR)
