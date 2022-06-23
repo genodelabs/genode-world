6027fc6c5b16d86e918099249e18c82f72077c00
tor-m6 <tor@innopolis.ru>
2022-06-24 00:56:44 +0300

    go: cross "go build" env with  goroot/gopath
    Issue #293

---
+++ src/app/goroot/target.mk
@@ -0,0 +1,21 @@
+TARGET = goroot
+
+GOOS:=inno
+GOARCH:=arm64
+
+GOROOT_PORT_DIR := $(call select_from_ports,goroot)
+GOROOT_DIR      := $(GOROOT_PORT_DIR)/src/app/goroot
+
+env.sh: $(PRG_DIR)/target.mk
+	$(VERBOSE)rm -f $@
+	$(VERBOSE)echo "unset CC CXX LD AR NM RANLIB STRIP CPPFLAGS"   >> $@
+	$(VERBOSE)echo "unset LIBTOOLFLAGS GCCGO CFLAGS LDFLAGS LIBS"  >> $@
+	$(VERBOSE)echo "export GOROOT='$(BUILD_BASE_DIR)/app/goroot'" >> $@
+	# avoid most of the tests
+	$(VERBOSE)echo "export GOTESTONLY='!'" >> $@
+
+$(TARGET): prepare_goroot
+	$(VERBOSE)source ./env.sh && cd src && ./all.bash
+
+prepare_goroot: env.sh
+	$(VERBOSE)cp -aprsf $(GOROOT_DIR)/* .
