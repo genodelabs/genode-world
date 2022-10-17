e4bdf9f1e9b3baa4103603a0a443a5d393924e2d
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2022-10-17 17:51:21 +0200

    libmbim: copy contrib sources correctly
    The src/lib/libmbim target is skipped if src/lib/libmbim/config.h was copied
    before copying the contrib code. Changing the order in the content rule
    fixes this.
    
    Moreover, add src/lib/libmbim_generated as a separate target for more
    robustness.
    
    Fixed #306

--- recipes/src/libmbim/content.mk
+++ recipes/src/libmbim/content.mk
@@ -1,6 +1,6 @@
 MIRROR_FROM_REP_DIR = lib/import/import-libmbim.mk lib/mk/libmbim.mk src/lib/libmbim/config.h
 
-content: $(MIRROR_FROM_REP_DIR) src/lib/libmbim LICENSE
+content: src/lib/libmbim src/lib/libmbim_generated $(MIRROR_FROM_REP_DIR) LICENSE
 
 $(MIRROR_FROM_REP_DIR):
 	$(mirror_from_rep_dir)
@@ -10,8 +10,10 @@ GENERATED_PORT_DIR := $(call port_dir,$(REP_DIR)/ports/libmbim_generated)
 
 src/lib/libmbim:
 	mkdir -p src/lib/libmbim
-	mkdir -p src/lib/libmbim_generated
 	cp -r $(PORT_DIR)/src/lib/libmbim/* src/lib/libmbim
+
+src/lib/libmbim_generated:
+	mkdir -p src/lib/libmbim_generated
 	cp -r $(GENERATED_PORT_DIR)/src/lib/libmbim_generated/* src/lib/libmbim_generated
 
 LICENSE:
