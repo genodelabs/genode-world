808b33c506c9c40b04d3fff1e172eb97ce8e29b9
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2023-05-15 11:27:17 +0200

    test/vncserver: fix example.o no such file
    Apparently, the LIBVNC_PORT_DIR variable defined in the
    import-vncserver.mk file cannot be used in the vpath directive.
    
    genodelabs/genode-world#327

--- src/test/vncserver/target.mk
+++ src/test/vncserver/target.mk
@@ -1,6 +1,8 @@
 TARGET = test-vncserver
 LIBS   = libc vncserver zlib posix
 
+LIBVNC_PORT_DIR := $(call select_from_ports,libvnc)
+
 INC_DIR  += $(LIBVNC_PORT_DIR)/src/lib/vnc/examples
 SRC_C = example.c
 
