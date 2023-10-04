02bd339e19a62816b079b85c83359d9cf194adb0
Christian Helmuth <christian.helmuth@genode-labs.com>
2023-10-04 15:48:30 +0200

    Stable TAR archive metadata content in recipes
    Differences in TAR archive member metadata results in unstable depot
    hashes. The following properties have to be fixed: modification time
    (incl. time zone), numeric owner and group, permission modes.
    
    Releated to genodelabs/genode#2842

--- recipes/raw/tyrian/content.mk
+++ recipes/raw/tyrian/content.mk
@@ -4,5 +4,7 @@ content: tyrian.tar
 
 PORT_DIR := $(call port_dir,$(REP_DIR)/ports/opentyrian)/src/app/opentyrian
 
+TAR_OPT := --owner=0 --group=0 --numeric-owner --mode='go=' --mtime='1970-01-01 00:00+00'
+
 tyrian.tar:
-	$(VERBOSE) tar cf $@ -C $(PORT_DIR)/tyrian21 .
+	$(VERBOSE) tar $(TAR_OPT) -cf $@ -C $(PORT_DIR)/tyrian21 .
