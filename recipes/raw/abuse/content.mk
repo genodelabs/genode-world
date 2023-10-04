38182d804c0edb42c6f1b9ba3605f03554383959
Christian Helmuth <christian.helmuth@genode-labs.com>
2023-10-04 15:48:30 +0200

    Stable TAR archive metadata content in recipes
    Differences in TAR archive member metadata results in unstable depot
    hashes. The following properties have to be fixed: modification time
    (incl. time zone), numeric owner and group, permission modes.
    
    Releated to genodelabs/genode#2842

--- recipes/raw/abuse/content.mk
+++ recipes/raw/abuse/content.mk
@@ -2,8 +2,10 @@ content: abuse.tar README
 
 ABUSE_DIR := $(call port_dir,$(REP_DIR)/ports/abuse)/src/app/abuse
 
+TAR_OPT := --owner=0 --group=0 --numeric-owner --mode='go=' --mtime='1970-01-01 00:00+00'
+
 abuse.tar: $(ABUSE_DIR)/data
-	tar cf $@ -C $< .
+	tar $(TAR_OPT) -cf $@ -C $< .
 
 README: $(ABUSE_DIR)/COPYING
 	cp $< $@
--- recipes/raw/glmark2/content.mk
+++ recipes/raw/glmark2/content.mk
@@ -2,5 +2,7 @@ content: glmark2_assets.tar
 
 PORT_DIR := $(call port_dir,$(REP_DIR)/ports/glmark2)/src/app/glmark2
 
+TAR_OPT := --owner=0 --group=0 --numeric-owner --mode='go=' --mtime='2021-04-29 00:00+00'
+
 glmark2_assets.tar:
-	tar --mtime='2021-04-29 00:00Z' -cf $@ -C $(PORT_DIR)/data .
+	tar $(TAR_OPT) -cf $@ -C $(PORT_DIR)/data .
