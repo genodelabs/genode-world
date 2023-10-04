0272ca5f41cacdb6792b98bee350c745b7785763
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2023-10-04 11:16:42 +0200

    depot: add recipe for udp_log and log_udp
    genodelabs/genode-world#343

---
+++ recipes/src/log_udp/content.mk
@@ -0,0 +1,2 @@
+SRC_DIR = src/proxy/log_udp
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc
---
+++ recipes/src/udp_log/content.mk
@@ -0,0 +1,2 @@
+SRC_DIR = src/proxy/udp_log
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc
