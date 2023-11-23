ba76337321a8130ef73211f4d133c21b91aac7d1
Norman Feske <norman.feske@genode-labs.com>
2023-11-23 14:05:08 +0100

    sdl_image/ttf: check port at libdep stage

--- lib/import/import-sdl_image.mk
+++ lib/import/import-sdl_image.mk
@@ -1,2 +1,2 @@
-INC_DIR += $(call select_from_ports,sdl_image)/include \
-           $(call select_from_ports,sdl_image)/include/SDL
+SDL_IMAGE_PORT_DIR := $(call select_from_ports,sdl_image)
+INC_DIR += $(addprefix $(SDL_IMAGE_PORT_DIR)/,include include/SDL)
