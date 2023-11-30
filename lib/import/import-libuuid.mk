50cd8982246deb5b83fe9c6db7eab49e52788254
Stefan Thöni <stefan.thoeni@gapfruit.com>
2023-11-30 14:25:25 +0100

    libuuid: change include patch
    tpm2-tss 4.0.x requires libuuid at the include path <uuid/uuid.h>
    which was not provided before this commit.
    
    Issue: gapfruit#1964
    Issue: genodelabs#384

--- lib/import/import-libuuid.mk
+++ lib/import/import-libuuid.mk
@@ -1,5 +1,7 @@
-UTIL_LINUX_DIR := $(call select_from_ports,util-linux)/src/lib/util-linux
+PORT_DIR := $(call select_from_ports,util-linux)
+UTIL_LINUX_DIR := $(PORT_DIR)/src/lib/util-linux
 LIBUUID_DIR := $(UTIL_LINUX_DIR)/libuuid/src
 
 INC_DIR += $(LIBUUID_DIR)
 INC_DIR += $(UTIL_LINUX_DIR)/include
+INC_DIR += $(PORT_DIR)/include
